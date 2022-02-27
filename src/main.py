0e65b820af8ed3909bdb57046c4d67c5a5ea5ae2
biffyclyro <joao-senna1@hotmail.com>
2022-02-26 22:58:02 -0300

    lib path

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 import urllib.request as req
+from pathlib import Path
 import json
 import os
 
 url = 'https://mtgarena.downloads.wizards.com/Live/Windows64/version'
+
+home = str(Path.home())
 
 resp = req.urlopen(url)
 
@@ -10,10 +13,11 @@ game_infos = json.loads(resp.read())
 
 game_file = req.urlopen(game_infos['CurrentInstallerURL'])
 
-os.system("rm -rf ./installer")
-os.system("mkdir ./installer")
+os.system(f"rm -rf {home}/Downloads/installer")
+os.system(f"mkdir {home}/Downloads/installer")
+os.system(f"touch {home}/Downloads/installer/magic.msi")
 
-with open('./installer/magic.msi', 'wb') as magic:
+with open(f'{home}/Downloads/installer/magic.msi', 'wb') as magic:
     magic.write(game_file.read())
 
-os.system("wine ./installer/magic.msi")
+os.system(f"wine {home}/Downloads/installer/magic.msi")
